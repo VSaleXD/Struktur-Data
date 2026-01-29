4f816d6b475e409071e2bb3193100ed98cb54861
VSaleXD <aleandremoulidouane@apps.ipb.ac.id>
2026-01-29 16:17:27 +0700

    hai

--- LKP1.cpp
+++ LKP1.cpp
@@ -8,19 +8,16 @@ struct Mahasiswa {
 };
 
 void cetak(Mahasiswa mhs[], int n){
-    for(int i=0;i<n;i++){
-        printf("Data Mahasiswa ke-%d:\n", i+1);
-        printf("Nama: %s\n", mhs[i].nama);
-        printf("NIM: %d\n", mhs[i].nim);
-        printf("IPK: %.2f\n", mhs[i].ipk);
-    }
+    printf("Nama: %s\n", mhs[n].nama);
+    printf("NIM: %d\n", mhs[n].nim);
+    printf("IPK: %.2f\n", mhs[n].ipk);
 }
 
 void cari(Mahasiswa mhs[], int n, int nimCari){
     for(int i=0;i<n;i++){
         if(mhs[i].nim == nimCari){
             printf("Data Mahasiswa ditemukan:\n");
-            cetak(&mhs[i], 1);
+            cetak(mhs, i);
             return;
         }
     }
@@ -34,19 +31,25 @@ int main() {
     mhs[0].nim = 12345;
     mhs[0].ipk = 3.75f;
 
-    cetak(mhs, 1);
+    cetak(mhs, 0);
 
     vector<Mahasiswa> vecMhs;
     vecMhs.push_back(mhs[0]);
 
     int idx=1;
+
+    printf("Masukkan data 2 mahasiswa baru (nama nim ipk):\n");
     for(int i=1;i<3;i++){
         cin>>mhs[idx].nama>>mhs[idx].nim>>mhs[idx].ipk;
         idx++;
     }
 
-    cari(mhs, idx, 23456);
-    cari(mhs, idx, 34567);
+    printf("Pencarian data mahasiswa berdasarkan NIM:\n");
+    int nimCari;
+    for(int i=0;i<2;i++){
+        cin>>nimCari;
+        cari(mhs, idx, nimCari);
+    }
 
     return 0;
 }
